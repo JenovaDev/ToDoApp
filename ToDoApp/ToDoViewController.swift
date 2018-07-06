aae739b57be21d846a1f5d289d48094ec9631f02
Ged Guillaume <gedg@geds-mac-mini.local>
2018-07-06 19:26:41 -0400

    ToDo List Complete with datasource and delegates

---
+++ ToDoApp/ToDoViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Ged Guillaume on 7/5/18.
+//  Copyright © 2018 Ged Guillaume. All rights reserved.
+//
+
+import UIKit
+
+class ToDoViewController: UITableViewController {
+
+    let itemArray = ["Clean Desk Area", "Go to Gym", "Learn iOS Development", "Do Laundry"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //MARK - Tableview Datasource Methods
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK - TableView Delegate Method
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+       
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+        tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        //unhighlights row for UI
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
